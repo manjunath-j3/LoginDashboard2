22862f8d273db6d4b127863b9b4c5ab504326ba3
manjunath.jagadeeshaiah <manjunath.j3@gmail.com>
2018-05-13 20:53:30 +0530

    Added redux code

--- script.jsx
+++ script.jsx
@@ -1,10 +1,22 @@
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.css";
+import { createStore, applyMiddleware} from 'redux';
+import { Provider} from "react-redux";   //to connect react and redux
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./rootReducer";
+
+const store = createStore(
+  rootReducer, 
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <BrowserRouter>
-      <App/>
+      <Provider>
+        <App/>
+      </Provider>
     </BrowserRouter>,
     document.getElementById('content')
 );
